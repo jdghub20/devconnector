64446afd2e3ed0571d23a0e0eec8cd5803a51c23
jdghub20 <joedias@hotmail.com>
2020-12-15 22:20:21 -0500

    updated profiles to get all profiles and by user id

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//const ObjectId = mongoose.Types.ObjectId;
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const mongoose = require('mongoose');
 
 // @route       GET api/profile/me
 // @desc        Get current users profile
@@ -107,4 +108,46 @@ router.post(
             res.status(500).send('Server Error');
         }
     });
+
+// @route       Get api/profile
+// @desc        Get all profiles
+// @access      Private
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+// @route       Get api/profile/user/:user_id
+// @desc        Get profile by user ID
+// @access      Private
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+
+    } catch (err) {
+
+        if (err.kind == 'ObjectId') {
+            // console.error(err.message);
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
